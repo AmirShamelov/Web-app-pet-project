6ffef9ae8430b7f253a6ff148937321271e222cf
AmirShamelov <ashamelov@gmail.com>
2024-05-22 17:48:36 +0600

    First commit, index and about

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context: dict = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME"
+    }
+
+    return render(request, 'main/index.html', context)
+def about(request):
+    context: dict = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Хороший товар"
+    }
+
+    return render(request, 'main/about.html', context)
+
+
+
