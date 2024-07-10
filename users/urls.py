a5fc7f4b6bd745bdc972cdd7fc65fa4ce07befce
AmirShamelov <ashamelov@gmail.com>
2024-07-10 18:29:33 +0600

    All cart templates are included

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('registration/', views.registration, name='registration'),
+    path('users-cart/', views.users_cart, name='users_cart'),
     path('profile/', views.profile, name='profile'),
 ]
