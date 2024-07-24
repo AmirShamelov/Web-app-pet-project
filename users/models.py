6d6bac8d568bbb92961d321dcffd7e2eec1417e1
AmirShamelov <ashamelov@gmail.com>
2024-07-25 01:17:01 +0600

    Order is ready

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Аватар')
+    phone_number = models.CharField(max_length=10, blank=True, null=True, verbose_name='Номер телефона')
 
     class Meta:
         db_table = 'user'
